4565008753dba72ecf05fe2412983a357fc14a45
LokeshC2 <47241840+LokeshC2@users.noreply.github.com>
2023-04-24 13:00:50 +0530

    server setup create and join endpoints

--- Server/server.js
+++ Server/server.js
@@ -6,6 +6,8 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const meetings = {};
+
 // use the Express JSON middleware
 app.use(express.json());
 
@@ -35,14 +37,14 @@ const findOrCreateRoom = async (roomName) => {
   }
 };
 
-const getAccessToken = (roomName) => {
+const getAccessToken = (roomName, participantId) => {
   // create an access token
   const token = new AccessToken(
     process.env.TWILIO_ACCOUNT_SID,
     process.env.TWILIO_API_KEY_SID,
     process.env.TWILIO_API_KEY_SECRET,
     // generate a random unique identity for this participant
-    { identity: uuidv4() }
+    { identity: participantId }
   );
   // create a video grant for this specific room
   const videoGrant = new VideoGrant({
@@ -55,18 +57,100 @@ const getAccessToken = (roomName) => {
   return token.toJwt();
 };
 
-app.post("/join-room", async (req, res) => {
-  // return 400 if the request has an empty body or no roomName
-  if (!req.body || !req.body.roomName) {
-    return res.status(400).send("Must include roomName argument.");
+app.post("/create-meeting", async (req, res) => {
+  // return 400 if the request has an empty body or no meetingName
+  if (!req.body || !req.body.meetingName) {
+    return res.status(400).send("Must include meetingName argument.");
   }
-  const roomName = req.body.roomName;
-  // find or create a room with the given roomName
-  findOrCreateRoom(roomName);
-  // generate an Access Token for a participant in this room
-  const token = getAccessToken(roomName);
+  const meetingName = req.body.meetingName;
+  const meetingId = uuidv4();
+
+  meetings[meetingId] = {
+    meetingName: meetingName,
+    participants: [],
+    rooms: [],
+    lobby: "",
+    details: req.body.details
+  };
+
   res.send({
+    meetingId: meetingId
+  });
+});
+
+app.post("/join", async (req, res) => {
+  let roomName;
+  let token;
+
+  // return 400 if the request has an empty body or no meetingId
+  if (!req.body || !req.body.meetingId) {
+    return res.status(400).send("Must include meetingId argument.");
+  }
+  const meetingId = req.body.meetingId;
+
+  // return 404 if the meetingId is not found
+  const meeting = meetings[meetingId];
+  if (!meeting) {
+    return res.status(404).send("Meeting not found.");
+  }
+
+  // return 400 if the request has an empty body or no participant
+  if (!req.body || !req.body.participant) {
+    return res.status(400).send("Must include participant argument.");
+  }
+
+  const participant = req.body.participant;
+
+  // if participant sends a request with a participantId, check if they are already in the meeting and return their token
+  if (participant.id && meeting.participants.length > 0 ) {
+    let p = meeting.participants.find(participant => participant.id === participant.id);
+    if (p) {
+      res.send({
+        participantId: p.id,
+        roomName: p.roomName,
+        token: getAccessToken(p.roomName, participant.id),
+        details: meetings[meetingId].details
+      });
+      return;
+    }
+  }
+
+  if (meetings[meetingId].lobby) {
+    // if lobby is not empty, join the first room in the lobby
+    roomName = meetings[meetingId].lobby;
+    meetings[meetingId].lobby = "";
+  } else {
+    // if lobby is empty, create a new room
+    roomName = uuidv4();
+    meetings[meetingId].lobby = roomName;
+  }
+
+  // find or create a room with the given roomName
+  try {
+    findOrCreateRoom(roomName);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Error creating room.");
+  }
+
+  //generate a new participantId
+  participant.id = uuidv4();
+
+  // generate an Access Token for a participant in this room
+  token = getAccessToken(roomName, participant.id);
+  
+  //update participant with roomName and token
+  participant.roomName = roomName;
+  participant.token = token;
+
+  //add participant to participants array
+  meetings[meetingId].participants.push(participant);
+
+  res.send({
+    participantId: participant.id,
     token: token,
+    roomName: roomName,
+    details: meetings[meetingId].details
   });
 });
 
