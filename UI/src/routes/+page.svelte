9d74160c292092712ddcf90124b9564ce5f4afbc
LokeshC2 <47241840+LokeshC2@users.noreply.github.com>
2023-04-24 11:08:36 +0530

    Added Svelte Template and server init

---
+++ UI/src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Create or join a meeting</h1>
+<!-- Use this page to make the UI for creating or joining a meeting. -->
---
+++ UI/src/routes/[meetingId]/+page.svelte
@@ -0,0 +1,21 @@
+<!-- Use this as the page to render when joined in a meeting. The meeting ID variable will be set automatically from URL. -->
+
+<script>
+    export let meetingId;
+
+    import { onMount } from 'svelte';
+
+</script>
+
+<!DOCTYPE html>
+<html lang='en-US'>
+  <head>
+    <meta charset="utf-8" />
+    <title>Twilio Video Demo</title>
+    <!-- Twilio Video CDN -->
+    <script src="https://sdk.twilio.com/js/video/releases/2.15.2/twilio-video.min.js"></script>
+  </head>
+  <body>
+    <div id="video-container"></div>
+  </body>
+</html>
