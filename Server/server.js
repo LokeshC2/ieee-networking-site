06b3d9902b96e4548074e0ef269878ae0c55ba9b
LokeshC2 <47241840+LokeshC2@users.noreply.github.com>
2023-06-13 19:47:41 +0530

    new-dry-run-no-ui

--- Server/server.js
+++ Server/server.js
@@ -1,15 +1,14 @@
 const { createServer } = require('http');
-const { io } = require('socket.io');
+const { Server } = require('socket.io');
 
-const server = createServer();
-const socketServer = new io(server);
+const httpServer = createServer();
+const socketServer = new Server(httpServer, { cors: { origin: '*' } });
+const PORT = process.env.PORT || 5000;
 
 const meetings = {};
 
-
 // type meetings = {
 //   [meetingId: string]: {
-//     host: string,
 //     startTime: number,
 //     shuffles: number,
 //     duration: number,
@@ -35,68 +34,164 @@ socketServer.on('connection', (socket) => {
   socket.emit('connected');
 
   socket.on('create-meeting', ({ host, startTime, shuffles, duration }) => {
-    console.log('Create meeting');
+    try {
+      console.log('Create meeting');
 
-    const meetingId = random_digits(6);
-    meetings[meetingId] = {
-      host,
-      startTime,
-      shuffles,
-      duration,
-      participants: {},
-    };
+      const now = Date.now();
 
-    socket.emit('meeting-created', { meetingId });
-  });
+      const meetingId = random_digits(6);
+      meetings[meetingId] = {
+        status: 'scheduled', // 'scheduled' | 'started' | 'ended
+        host,
+        startTime,
+        shuffles,
+        duration,
+        participants: {},
+      };
 
-  socket.on('join-meeting', ({ meetingId, participant }) => {
-    const { name, email } = participant;
-    console.log('Join meeting');
-    if (!meetings[meetingId]) {
-      socket.emit('meeting-not-found');
+      setTimeout(startMeeting, startTime - now, meetingId, startTime);
+
+      setTimeout(() => {
+        meetings[meetingId].status = 'ended';
+        socketServer.to(meetingId).emit('meeting-ended');
+      }, startTime + shuffles * duration - now);
+
+      socket.emit('meeting-created', meetingId);
+
+    } catch (error) {
+      socket.emit('error', error.message);
       return;
     }
+  });
 
-    const participantId = random_digits(3);
-    meetings[meetingId].participants[participantId] = {
-      name,
-      email,
-      socketId: socket.id,
-    };
+  socket.on('join-meeting', ({ meetingId, participant: { name, email } }) => {
+    // VALIDATE INPUTS
 
-    socket.emit('meeting-joined', {
-      status: meetings[meetingId].status,
-      participantId
-    });
+    try {
+      console.log('Join meeting');
+      if (!meetings[meetingId]) {
+        socket.emit('meeting-not-found');
+        throw new Error('Meeting not found');
+      }
+
+      const participantId = random_digits(3);
+      meetings[meetingId].participants[participantId] = {
+        name,
+        email,
+        socketId: socket.id,
+      };
+
+      socket.join(meetingId);
+      socket.join(participantId);
+
+      socket.emit('meeting-joined', {
+        status: meetings[meetingId].status,
+        participantId
+      });
+
+    } catch (error) {
+      socket.emit('error', error.message);
+      return;
+    }
   });
 
   socket.on('get-room', ({ meetingId, participantId }) => {
-    console.log('Get room');
-    if (!meetings[meetingId]) {
-      socket.emit('meeting-not-found');
+
+    try {
+      console.log('Get room');
+      if (!meetings[meetingId]) {
+        socket.emit('meeting-not-found');
+        throw new Error('Meeting not found');
+      }
+
+      if (!meetings[meetingId].participants[participantId]) {
+        socket.emit('participant-not-found');
+        throw new Error('Participant not found');
+      }
+
+      socket.emit('room', meetings[meetingId].participants[participantId].room);
+
+    } catch (error) {
+      socket.emit('error', error.message);
       return;
     }
-
-    if (!meetings[meetingId].participants[participantId]) {
-      socket.emit('participant-not-found');
-      return;
-    }
-
-    socket.emit('room', {
-      meetingId,
-      participantId,
-      room: meetings[meetingId].participants[participantId].room,
-      token: meetings[meetingId].participants[participantId].token,
-      partner: meetings[meetingId].participants[participantId].partner,
-      closingTime: meetings[meetingId].participants[participantId].closingTime,
-    });
-
-    socket.on('disconnect', () => {
-      console.log('User disconnected');
-    });
   });
-  
 
-  function random_digits(n) {
-    return Math.floor(Math.random() * 10 ** n).toString().padStart(n, "0");
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+function random_digits(n) {
+  return Math.floor(Math.random() * 10 ** n).toString().padStart(n, "0");
+}
+
+function startMeeting(meetingId, startTime) {
+  if (meetings[meetingId] === undefined) {
+    throw new Error('Meeting not found');
   }
+  meetings[meetingId].status = 'started';
+  socketServer.to(meetingId).emit('meeting-started');
+  for (let i = 0; i < meetings[meetingId].shuffles; i++) {
+    setTimeout(
+      shuffle,
+      i * meetings[meetingId].duration,
+      meetingId,
+      startTime + (i + 1) * meetings[meetingId].duration
+    );
+  }
+  setTimeout(endMeeting, meetings[meetingId].shuffles * meetings[meetingId].duration, meetingId);
+}
+
+function shuffle(meetingId, closingTime) {
+
+  const participants = Object.keys(meetings[meetingId].participants);
+  const shuffled = participants.sort(() => Math.random() - 0.5);
+
+  for (let i = 0; i < (shuffled.length-1)/2; i++) {
+
+    const roomId = random_digits(6);
+    const participantId = shuffled.pop();
+    const partnerId = shuffled.pop();
+
+    meetings[meetingId].participants[participantId].room = {
+      roomId,
+      token: random_digits(6),
+      partner: partnerId,
+      closingTime,
+    };
+
+    socketServer.to(participantId).emit('assigned-room', meetings[meetingId].participants[participantId].room);
+
+    meetings[meetingId].participants[partnerId].room = {
+      roomId,
+      token: random_digits(6),
+      partner: participantId,
+      closingTime,
+    };
+
+    socketServer.to(partnerId).emit('assigned-room', meetings[meetingId].participants[partnerId].room);
+  }
+
+  if (shuffled.length === 1) {
+    const participantId = shuffled.pop();
+    meetings[meetingId].participants[participantId].room = {
+      roomId: null,
+      token: null,
+      partner: null,
+      closingTime,
+    };
+    socketServer.to(participantId).emit('no-partner', closingTime);
+  }
+
+}
+
+function endMeeting(meetingId) {
+  meetings[meetingId].status = 'ended';
+  socketServer.to(meetingId).emit('meeting-ended');
+}
+
+
+httpServer.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
--- newUI/src/App.svelte
+++ newUI/src/App.svelte
@@ -3,64 +3,142 @@
   import Join from "./lib/Join.svelte";
   import Share from "./lib/Share.svelte";
   import Meeting from "./lib/Meeting.svelte";
-  
+
   import { io } from "socket.io-client";
   import { onMount } from "svelte";
 
   let meetingId;
+  let participantId;
+  
+  let closingTime;
+  let remainingTime
+
+  setInterval(() => {
+    if (closingTime) {
+      remainingTime = Math.max(0, Math.floor((closingTime - Date.now()) / 1000));
+    }
+  }, 1000);
+  
   let joined = false;
 
   let socket;
 
-  onMount(()=>{
-    socket = io();
-  })
+  let message;
 
+  onMount(async () => {
+    meetingId = window.location.pathname.split("/")[2] || null;
+
+    message = "Waiting for server";
+    setTimeout(() => {
+      socket = io("http://localhost:3000");
+
+      socket.on("connected", () => {
+        message = "Connected to server";
+      });
+
+      socket.on("error", (err) => {
+        message = "Error: " + err;
+      });
+
+      socket.on("meeting-created", (mId) => {
+        message = "Meeting created";
+        meetingId = mId;
+      });
+
+      socket.on("meeting-joined", ({ status, participantId: pId }) => {
+        participantId = pId;
+        message = "Meeting joined with status: " + status;
+        history.replaceState({}, "", `/meeting/${meetingId}`);
+        joined = true;
+        if (status === "started") {
+          message += "\nMeeting already started, waiting for next shuffle";
+        }
+      });
+
+      socket.on("meeting-started", () => {
+        message = "Meeting started";
+      });
+
+      socket.on("assigned-room", (room) => {
+        closingTime = room.closingTime;
+        message += "\nAssigned room" + JSON.stringify(room);
+      });
+
+      socket.on("no-partner", (t) => {
+        message = "No partner found";
+        closingTime = t;
+      });
+
+      socket.on("meeting-ended", () => {
+        message = "Meeting ended";
+      });
+
+      socket.on("disconnect", () => {
+        message = "Disconnected from server";
+      });
+    }, 3000);
+  });
+
+  function createMeeting() {
+    socket.emit("create-meeting", {
+      host: {
+        name: "dummy",
+        email: "x@example.com",
+      },
+      startTime: Date.now() + 0.25 * 60 * 1000,
+      shuffles: 2,
+      duration: 60 * 1000,
+    });
+  }
+
+  function joinMeeting() {
+    socket.emit("join-meeting", {
+      meetingId,
+      participant: {
+        name: "dummy",
+        email: "y@example.com",
+      },
+    });
+  }
+
+  function shareMeeting() {
+    window.navigator.clipboard.writeText(
+      window.location.origin + "/meeting/" + meetingId
+    );
+    alert("Meeting Link copied to clipboard");
+  }
 </script>
 
 <main>
   <div>
-    <h1>Networking Site</h1>
-
-    {#if !meetingId}
-      <div class="card">
-        <Create />
-      </div>
+    {message}
+    {#if meetingId}
+      <p>Meeting ID: {meetingId}</p>
+      {#if joined}
+        <p>Joined</p>
+      {/if}
     {/if}
-
-    <div class="card">
-      <Join meetingId />
-    </div>
-
-    {#if meetingId && !joined}
-      <div class="card">
-        <Share meetingId />
-      </div>
+    <hr />
+    <h4>Create</h4>
+    <button on:click={createMeeting}>Create Meeting</button>
+    <hr />
+    <h4>Join</h4>
+    <button on:click={joinMeeting}>Join Meeting</button>
+    {#if participantId}
+      <p>Participant ID: {participantId}</p>
     {/if}
-
-    {#if meetingId && joined}
-      <div class="card">
-        <Meeting meetingId />
-      </div>
+    <hr />
+    <h4>Share</h4>
+    <button on:click={shareMeeting}>Share Meeting</button>
+    <hr />
+    {#if closingTime}
+    <h4>Room-time remaining</h4>
+      <p>Remaining time: {remainingTime}</p>
     {/if}
+  </div>
 
+  <div id="video">
+    <div id="local">
+      
   </div>
 </main>
-
-<style>
-  /* .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  } */
-</style>
