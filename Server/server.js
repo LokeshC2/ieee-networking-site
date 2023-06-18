11b695ed4f54c39d5fef2abed4fe02acaec2febb
LokeshC2 <47241840+LokeshC2@users.noreply.github.com>
2023-06-18 19:23:43 +0530

    back to http

--- Server/server.js
+++ Server/server.js
@@ -1,9 +1,15 @@
 const { createServer } = require('http');
-const { Server } = require('socket.io');
+// const { Server } = require('socket.io');
 
 const httpServer = createServer();
 const socketServer = new Server(httpServer, { cors: { origin: '*' } });
 const PORT = process.env.PORT || 5000;
+
+const express = require('express')
+const cors = require('cors')
+const app = express();
+app.use(cors())
+app.use(express.json())
 
 const meetings = {};
 
@@ -29,98 +35,123 @@ const meetings = {};
 // }
 
 
-socketServer.on('connection', (socket) => {
-  console.log('User connected');
-  socket.emit('connected');
+// socketServer.on('connection', (socket) => {
+//   console.log('User connected');
+//   socket.emit('connected');
 
-  socket.on('create-meeting', ({ host, startTime, shuffles, duration }) => {
-    try {
-      console.log('Create meeting');
+// socket.on('create-meeting', ({ host, startTime, shuffles, duration }) => {
+app.post('/create-meeting', (req, res) => {
+  try {
 
-      const now = Date.now();
+    const { host, startTime, shuffles, duration } = req.body
 
-      const meetingId = random_digits(6);
-      meetings[meetingId] = {
-        status: 'scheduled', // 'scheduled' | 'started' | 'ended
-        host,
-        startTime,
-        shuffles,
-        duration,
-        participants: {},
-      };
+    const meetingId = random_digits(6);
+    const now = Date.now();
 
-      setTimeout(startMeeting, startTime - now, meetingId, startTime);
+    console.log(`Created meeting ${meetingId} by ${host.name}`)
 
-      setTimeout(() => {
-        meetings[meetingId].status = 'ended';
-        socketServer.to(meetingId).emit('meeting-ended');
-      }, startTime + shuffles * duration - now);
+    meetings[meetingId] = {
+      status: 'scheduled', // 'scheduled' | 'started' | 'ended
+      host,
+      startTime,
+      shuffles,
+      duration,
+      participants: {},
+    };
 
-      socket.emit('meeting-created', meetingId);
+    setTimeout(startMeeting, startTime - now, meetingId, startTime);
 
-    } catch (error) {
-      socket.emit('error', error.message);
-      return;
-    }
-  });
+    setTimeout(() => {
+      meetings[meetingId].status = 'ended';
+      socketServer.to(meetingId).emit('meeting-ended');
+    }, startTime + shuffles * duration - now);
 
-  socket.on('join-meeting', ({ meetingId, participant: { name, email } }) => {
-    // VALIDATE INPUTS
+    // socket.emit('meeting-created', meetingId);
+    res.status(200).json({
+      meetingId
+    })
 
-    try {
-      console.log('Join meeting');
-      if (!meetings[meetingId]) {
-        socket.emit('meeting-not-found');
-        throw new Error('Meeting not found');
-      }
-
-      const participantId = random_digits(3);
-      meetings[meetingId].participants[participantId] = {
-        name,
-        email,
-        socketId: socket.id,
-      };
-
-      socket.join(meetingId);
-      socket.join(participantId);
-
-      socket.emit('meeting-joined', {
-        status: meetings[meetingId].status,
-        participantId
-      });
-
-    } catch (error) {
-      socket.emit('error', error.message);
-      return;
-    }
-  });
-
-  socket.on('get-room', ({ meetingId, participantId }) => {
-
-    try {
-      console.log('Get room');
-      if (!meetings[meetingId]) {
-        socket.emit('meeting-not-found');
-        throw new Error('Meeting not found');
-      }
-
-      if (!meetings[meetingId].participants[participantId]) {
-        socket.emit('participant-not-found');
-        throw new Error('Participant not found');
-      }
-
-      socket.emit('room', meetings[meetingId].participants[participantId].room);
-
-    } catch (error) {
-      socket.emit('error', error.message);
-      return;
-    }
-  });
-
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
+  } catch (error) {
+    res.status(400).json({
+      error: error.message
+    })
+    return;
+  }
 });
+
+// socket.on('join-meeting', ({ meetingId, participant: { name, email } }) => {
+// VALIDATE INPUTS
+app.post('/join-meeting', (req, res) => {
+  try {
+    const { meetingId, participant: { name, email } } = req.body
+
+    if (!meetings[meetingId]) {
+      // socket.emit('meeting-not-found');
+      throw new Error('Meeting not found');
+    }
+
+    const participantId = random_digits(3);
+    meetings[meetingId].participants[participantId] = {
+      name,
+      email,
+      // socketId: socket.id,
+    };
+
+    // socket.join(meetingId);
+    // socket.join(participantId);
+
+    // socket.emit('meeting-joined', {
+
+    res.status(200).json({
+      meetingId,
+      participantId
+    })
+
+    console.log(`Participant ${participantId} joined meeting ${meetingId}`);
+  } catch (error) {
+    res.status(400).json({
+      error: error.message
+    })
+    console.log(error.message);
+    return;
+  }
+});
+
+// socket.on('get-room', ({ meetingId, participantId }) => {
+app.post('/get-room', (req, res) => {
+
+  try {
+    const { meetingId, participantId } = req.body
+    if (!meetings[meetingId]) {
+      // socket.emit('meeting-not-found');
+      throw new Error('Meeting not found');
+    }
+    
+    if (!meetings[meetingId].participants[participantId]) {
+      // socket.emit('participant-not-found');
+      throw new Error('Participant not found');
+    }
+    
+    // socket.emit('room', meetings[meetingId].participants[participantId].room);
+    
+    res.status(200).json({
+      room: meetings[meetingId].participants[participantId].room
+    })
+    
+    console.log('Get room');
+  } catch (error) {
+    // socket.emit('error', error.message)'';
+    res.status(400).json({
+      error: error.message
+    })
+    return;
+  }
+});
+
+// socket.on('disconnect', () => {
+//   console.log('User disconnected');
+// });
+// });
 
 function random_digits(n) {
   return Math.floor(Math.random() * 10 ** n).toString().padStart(n, "0");
@@ -148,7 +179,7 @@ function shuffle(meetingId, closingTime) {
   const participants = Object.keys(meetings[meetingId].participants);
   const shuffled = participants.sort(() => Math.random() - 0.5);
 
-  for (let i = 0; i < (shuffled.length-1)/2; i++) {
+  for (let i = 0; i < (shuffled.length - 1) / 2; i++) {
 
     const roomId = random_digits(6);
     const participantId = shuffled.pop();
@@ -161,7 +192,7 @@ function shuffle(meetingId, closingTime) {
       closingTime,
     };
 
-    socketServer.to(participantId).emit('assigned-room', meetings[meetingId].participants[participantId].room);
+    // socketServer.to(participantId).emit('assigned-room', meetings[meetingId].participants[participantId].room);
 
     meetings[meetingId].participants[partnerId].room = {
       roomId,
@@ -170,7 +201,7 @@ function shuffle(meetingId, closingTime) {
       closingTime,
     };
 
-    socketServer.to(partnerId).emit('assigned-room', meetings[meetingId].participants[partnerId].room);
+    // socketServer.to(partnerId).emit('assigned-room', meetings[meetingId].participants[partnerId].room);
   }
 
   if (shuffled.length === 1) {
@@ -181,14 +212,14 @@ function shuffle(meetingId, closingTime) {
       partner: null,
       closingTime,
     };
-    socketServer.to(participantId).emit('no-partner', closingTime);
+    // socketServer.to(participantId).emit('no-partner', closingTime);
   }
 
 }
 
 function endMeeting(meetingId) {
   meetings[meetingId].status = 'ended';
-  socketServer.to(meetingId).emit('meeting-ended');
+  // socketServer.to(meetingId).emit('meeting-ended');
 }
 
 
